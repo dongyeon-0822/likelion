de732525fe699538a7500a4506621f7ddd214270
DongYeon Kang <myjjue00@gmail.com>
2022-10-04 14:18:14 +0900

    Update CoinTest.java

--- src/CoinTest.java
+++ src/CoinTest.java
@@ -10,10 +10,18 @@ public class CoinTest {
         System.out.print("금액을 넣으세요 : ");
         amount = scanner.nextInt();
         
-        for (int i = 0; i < unit.length; i++){
-            num = amount / unit[i];
-            amount %= unit[i];
-            System.out.println(unit[i] + "원 " + num + "개");
-        }
+//        for (int i = 0; i < unit.length; i++){
+//            num = amount / unit[i];
+//            amount %= unit[i];
+//            System.out.println(unit[i] + "원 " + num + "개");
+//        }
+        System.out.println(unit[0] + "원 " + amount / unit[0] + "개");
+        amount %= unit[0];
+        System.out.println(unit[1] + "원 " + amount / unit[1] + "개");
+        amount %= unit[1];
+        System.out.println(unit[2] + "원 " + amount / unit[2] + "개");
+        amount %= unit[2];
+        System.out.println(unit[3] + "원 " + amount / unit[3] + "개");
+        amount %= unit[3];
     }
 }
