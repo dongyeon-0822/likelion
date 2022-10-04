7c3b3f9b29f219a85b91706de1d020d9eec500cb
DongYeon Kang <myjjue00@gmail.com>
2022-10-04 13:23:05 +0900

    Update ArrayTest.java

--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -3,7 +3,7 @@ public class ArrayTest {
         System.out.println("hello");
         // 10칸의 int array 를 만들고,
         // 1-10을 채우고, 출력하라.
-        Integer array[] = new Integer[10];
+        int[] array = new int[10];
 
         for (int i = 0; i < 10; i++){
             array[i] = i+1;
