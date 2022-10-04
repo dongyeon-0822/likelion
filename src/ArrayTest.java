2ea1feda90dabd90ab9de1f33e96f282f42936f2
DongYeon Kang <myjjue00@gmail.com>
2022-10-04 13:20:42 +0900

    Create ArrayTest.java

---
+++ src/ArrayTest.java
@@ -0,0 +1,15 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        // 10칸의 int array 를 만들고,
+        // 1-10을 채우고, 출력하라.
+        Integer array[] = new Integer[10];
+
+        for (int i = 0; i < 10; i++){
+            array[i] = i+1;
+        }
+        for (int i = 0; i < 10; i++){
+            System.out.println(array[i]);
+        }
+    }
+}
