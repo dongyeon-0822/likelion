799a6438facafd94943ab899631874b373a46d3a
DongYeon Kang <myjjue00@gmail.com>
2022-10-04 15:11:39 +0900

    Create EvenOdd.java (Solve CodeUp-1156)

---
+++ src/EvenOdd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        if (input % 2 == 0){
+            System.out.println("even");
+        }
+        else {
+            System.out.println("odd");
+        }
+    }
+}
