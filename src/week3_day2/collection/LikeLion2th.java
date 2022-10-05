782326fac2757d267b19da7f9da20a03ca9bde15
DongYeon Kang <myjjue00@gmail.com>
2022-10-05 20:47:18 +0900

    Create collection/LikeLion

--- src/week3_day2/collection/LikeLion2th.java
+++ src/week3_day2/collection/LikeLion2th.java
@@ -3,55 +3,20 @@ package week3_day2.collection;
 import java.util.ArrayList;
 import java.util.List;
 
+// 학생들의 이름, 정보를 반환하는 메소드를 가진 클래스
 public class LikeLion2th {
-    private List<String> students = new ArrayList<>();
-
+    Infos infos = new Infos();
+    private List<String> studentNames = new ArrayList<>();
+    private List<Student> studentObjs = new ArrayList<>();
     public LikeLion2th() {
-        this.students.add("안예은");
-        this.students.add("남우빈");
-        this.students.add("최경민");
-        this.students.add("안준휘");
-        this.students.add("하채민");
-        this.students.add("허도한");
-        this.students.add("배태훈");
-        this.students.add("정해성");
-        this.students.add("하찬희");
-        this.students.add("박도아");
-        this.students.add("유경준");
-        this.students.add("고다니");
-        this.students.add("하이안");
-        this.students.add("고유호");
-        this.students.add("전민찬");
-        this.students.add("배강");
-        this.students.add("유서이");
-        this.students.add("황도빈");
-        this.students.add("강규진");
-        this.students.add("임예린");
-        this.students.add("양예온");
-        this.students.add("곽윤재");
-        this.students.add("한단");
-        this.students.add("윤성민");
-        this.students.add("박재윤");
-        this.students.add("서현준");
-        this.students.add("노유주");
-        this.students.add("전율");
-        this.students.add("권아윤");
-        this.students.add("송태서");
-        this.students.add("정라온");
-        this.students.add("서서한");
-        this.students.add("문주호");
-        this.students.add("오세아");
-        this.students.add("이호영");
-        this.students.add("손윤아");
-        this.students.add("이동우");
-        this.students.add("남혜준");
-        this.students.add("황솔");
-        this.students.add("곽준휘");
-        this.students.add("양시현");
+        this.studentNames = infos.getStudentNames();
+        this.studentObjs = infos.getStudentObjs();
     }
-
     // 학생 이름 리스트를 리턴하는 메소드
-    public List<String> getStudentList() {
-        return this.students;
+    public List<String> getStudentNameList() {
+        return this.studentNames;
+    }
+    public List<Student> getStudentObjsList() {
+        return this.studentObjs;
     }
 }
--- src/week3_day2/collection/LikeLionMain.java
+++ src/week3_day2/collection/LikeLionMain.java
@@ -5,10 +5,16 @@ import java.util.List;
 public class LikeLionMain {
     public static void main(String[] args) {
         LikeLion2th likeLion2th = new LikeLion2th();
-        List<String> students = likeLion2th.getStudentList();
+        List<String> studentNames = likeLion2th.getStudentNameList();
+        List<Student> studentInfos = likeLion2th.getStudentObjsList();
 
-        for (String s : students){
-            System.out.println(s);
+        //학생들의 이름 출력
+        for (String name : studentNames){
+            System.out.println(name);
+        }
+        //학생들의 정보 출력
+        for (Student student : studentInfos){
+            System.out.printf("반 : %d, 이름 : %s, gitRepo : %s\n", student.getClassNo(), student.getName(), student.getGitRepoAddress());
         }
     }
 }
