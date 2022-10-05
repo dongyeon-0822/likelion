71569e524434a0a06dbe8c6286ad7059efceea68
DongYeon Kang <myjjue00@gmail.com>
2022-10-05 11:37:55 +0900

    Modify Exception

--- src/week3_day2/RandomCalculator.java
+++ src/week3_day2/RandomCalculator.java
@@ -19,10 +19,16 @@ public class RandomCalculator {
         System.out.println(a*b);
     }
     public void divide(){
-        try {
-            System.out.println((double) a / b);
-        } catch (Exception e) {
+        // error 가 발생하지 않고 Infinity 가 출력됨
+//        try {
+//            System.out.println((double) a / b);
+//        } catch (Exception e) {
+//            System.out.println("랜덤생성 숫자가 0입니다.");
+//        }
+        if (b == 0) {
             System.out.println("랜덤생성 숫자가 0입니다.");
+        } else {
+            System.out.println((double) a / b);
         }
     }
 }
