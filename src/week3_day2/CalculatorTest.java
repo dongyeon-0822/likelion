f59726350d680d695ed90eae55cf6cbe69ffa2e3
DongYeon Kang <myjjue00@gmail.com>
2022-10-05 10:24:02 +0900

    Create Calculator.java, CalculatorTest.java

---
+++ src/week3_day2/CalculatorTest.java
@@ -0,0 +1,11 @@
+package week3_day2;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        c.plus(1, 2);
+        c.minus(2, 3);
+        c.multiple(3,4);
+        c.divide(4,5);
+    }
+}
