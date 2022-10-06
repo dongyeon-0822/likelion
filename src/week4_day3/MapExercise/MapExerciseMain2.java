15b4a23cc7126e09a687aa437dad0465116f2210
DongYeon Kang <myjjue00@gmail.com>
2022-10-06 11:39:10 +0900

    Add MapExerciseMain2.java

---
+++ src/week4_day3/MapExercise/MapExerciseMain2.java
@@ -0,0 +1,22 @@
+package week4_day3.MapExercise;
+
+import java.util.HashMap;
+
+public class MapExerciseMain2 {
+    public static void main(String[] args) {
+        String repoAddr = "https://github.com/dongyeon-0822/java-project-exercise";
+
+        HashMap<Character, Integer> alphabetCnt = new HashMap<>();
+        for (Character c : repoAddr.toCharArray()) {
+            if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z') {
+                c = Character.toLowerCase(c);
+                if (alphabetCnt.containsKey(c)) {
+                    alphabetCnt.put(c, alphabetCnt.get(c) + 1);
+                } else {
+                    alphabetCnt.put(c, 1);
+                }
+            }
+        }
+        System.out.println(alphabetCnt);
+    }
+}
