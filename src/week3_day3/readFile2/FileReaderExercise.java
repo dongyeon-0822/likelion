31379f087f1f8008234d3db3e2a373ee53f59f56
DongYeon Kang <myjjue00@gmail.com>
2022-10-07 12:01:13 +0900

    Modify FileReaderExercise.java

--- src/week3_day3/readFile2/FileReaderExercise.java
+++ src/week3_day3/readFile2/FileReaderExercise.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class FileReaderExercise {
+    // 파일 디렉토리 출력
     public void printFiles(){
         File dir = new File("./");
         File files[] = dir.listFiles();
@@ -12,10 +13,12 @@ public class FileReaderExercise {
             System.out.println(file);
         }
     }
+    //파일에서 한글자 읽기
     public char readAChar(String filename) throws IOException {
         FileReader fileReader = new FileReader(filename);
         return (char)fileReader.read();
     }
+    //파일에서 두글자 읽기
     public String readTwoChar(String filename) throws IOException {
         FileReader fileReader = new FileReader(filename);
         String str = "";
@@ -25,8 +28,6 @@ public class FileReaderExercise {
     }
     public static void main(String[] args) throws IOException {
         FileReaderExercise fileReaderExercise = new FileReaderExercise();
-//        char c = fileReaderExercise.readAChar("test.txt");
-//        System.out.println(c);
         String str = fileReaderExercise.readTwoChar("test.txt");
         System.out.println(str);
     }
