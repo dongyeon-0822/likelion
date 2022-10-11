400d82551cdaa672b20efe8b1df9f3597a3a4564
DongYeon Kang <myjjue00@gmail.com>
2022-10-11 09:26:37 +0900

    Add solution_2

--- src/week4_day1/algorithm/Problem1.java
+++ src/week4_day1/algorithm/Problem1.java
@@ -1,12 +1,23 @@
 package week4_day1.algorithm;
 
 public class Problem1 {
-    public int solution(int n) {
+    public int solution_1(int n) {
         int answer = 0;
 
         String s = Integer.toString(n);
         for (char c : s.toCharArray()){
             answer += (c - '0');
+        }
+
+        return answer;
+    }
+    public int solution_2(int n)
+    {
+        int answer = 0;
+
+        while (n != 0) {
+            answer += n % 10;
+            n /= 10;
         }
 
         return answer;
