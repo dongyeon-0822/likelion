9609657136cc17ba08070b2fcd44162a7ddedeb7
DongYeon Kang <myjjue00@gmail.com>
2022-10-12 09:36:27 +0900

    Create Algorithm Max

---
+++ src/week4_day2/algorithm/Max.java
@@ -0,0 +1,19 @@
+package week4_day2.algorithm;
+
+public class Max {
+    public int getMax(int[] arr) {
+        int maxValue = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maxValue) {
+                maxValue = arr[i];
+            }
+        }
+        return maxValue;
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[] {3, 29, 38, 12,57, 74, 40, 85, 61};
+        Max max = new Max();
+        int maxNum = max.getMax(arr);
+        System.out.println(maxNum);
+    }
+}
