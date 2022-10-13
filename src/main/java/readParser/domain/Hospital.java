a8ff3528d85ceecccda6cd68a4af809ce55556d9
DongYeon Kang <myjjue00@gmail.com>
2022-10-13 14:05:30 +0900

    Add parsing district

--- src/main/java/readParser/domain/Hospital.java
+++ src/main/java/readParser/domain/Hospital.java
@@ -1,16 +1,21 @@
 package readParser.domain;
 
 public class Hospital {
-    private String id;
-    private String address;
-    private String district;
-    private String category;
-    private Integer emergencyRoom;
-    private String name;
-    private String subDivision;
+    private String id; //0
+    private String address; //1
+    private String district; //1 split
+    private String category; //2
+    private Integer emergencyRoom; //6
+    private String name; //10
+    private String subDivision; //10 split
 
     public Hospital(String id) {
         this.id = id;
+    }
+
+    public Hospital(String id, String address) {
+        this.id = id;
+        this.address = address;
     }
 
     public Hospital(String id, String address, String category, int emergencyRoom,
@@ -21,9 +26,37 @@ public class Hospital {
         this.name = name;
         this.emergencyRoom = emergencyRoom;
         this.subDivision = subDivision;
+        this.setDistrict();
+    }
+
+    private void setDistrict() {
+        String[] splitted = address.split(" ");
+        this.district = String.format("%s %s", splitted[0],splitted[1]);
+    }
+
+    private String replaceAllQuot(String str) {
+        return str.replaceAll("\"", "");
     }
 
     public String getId() {
         return id;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public String getDistrict(){
+        return district;
+    }
+    public int getEmergencyRoom() {
+        return emergencyRoom;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getSubdivision() {
+        return subDivision;
     }
 }
--- src/main/java/readParser/parser/HospitalParser.java
+++ src/main/java/readParser/parser/HospitalParser.java
@@ -7,6 +7,6 @@ public class HospitalParser implements Parser<Hospital>{
     @Override
     public Hospital parse(String str) {
         String[] splitted = str.split(",");
-        return new Hospital(splitted[0]);
+        return new Hospital(splitted[0], splitted[1], splitted[2], Integer.parseInt(splitted[6]), splitted[4], splitted[5]);
     }
 }
