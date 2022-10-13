6557c437b43221e7a9c87051e97217a6e346fea6
DongYeon Kang <myjjue00@gmail.com>
2022-10-13 11:36:50 +0900

    initial commit

---
+++ src/week4_day3/read_parser/parser/HospitalParser.java
@@ -0,0 +1,12 @@
+package week4_day3.read_parser.parser;
+
+
+import week4_day3.read_parser.domain.Hospital;
+
+public class HospitalParser implements Parser<Hospital>{
+    @Override
+    public Hospital parse(String str) {
+        String[] splitted = str.split(",");
+        return new Hospital(splitted[0]);
+    }
+}
---
+++ src/week4_day3/read_parser/parser/Parser.java
@@ -0,0 +1,5 @@
+package week4_day3.read_parser.parser;
+
+public interface Parser<T> {
+    T parse(String str);
+}
