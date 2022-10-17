b8b4027bd480b0625a04b7f3a704756a5989a1d8
DongYeon Kang <myjjue00@gmail.com>
2022-10-17 15:09:29 +0900

    Add User.java 1. Add select method

--- src/main/java/com/dbexercise/UserDao.java
+++ src/main/java/com/dbexercise/UserDao.java
@@ -1,16 +1,15 @@
 package com.dbexercise;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import com.dbexercise.domain.User;
+
+import java.sql.*;
 
 public class UserDao {
     public void add() throws SQLException, ClassNotFoundException {
         ConnectionMaker cm = new ConnectionMaker();
         Connection conn = cm.makeConnection();
 
-        String insertQuery = "INSERT INTO user INTO (?,?,?);";
+        String insertQuery = "INSERT INTO user(id, name, password) VALUES(?,?,?);";
         PreparedStatement ps = conn.prepareStatement(insertQuery);
         ps.setString(1,"2");
         ps.setString(2, "dongyeon2");
@@ -21,6 +20,25 @@ public class UserDao {
         conn.close();
         System.out.println("INSERT 성공");
     }
+
+    public User get(String id) throws SQLException, ClassNotFoundException {
+        ConnectionMaker cm = new ConnectionMaker();
+        Connection conn = cm.makeConnection();
+
+        String selectQuery = "SELECT * from user id=?;";
+        PreparedStatement ps = conn.prepareStatement(selectQuery);
+        ps.setString(1,id);
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User(rs.getString(1), rs.getString(2),rs.getString(3));
+
+        rs.close();
+        ps.close();
+        conn.close();
+        System.out.println("SELECT 성공");
+        return user;
+    }
+
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
