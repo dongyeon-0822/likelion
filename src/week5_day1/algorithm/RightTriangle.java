f20abe3c1743b0f821ef8dae2592fd19b445f750
DongYeon Kang <myjjue00@gmail.com>
2022-10-17 09:07:56 +0900

    Add RightTriangle.java

---
+++ src/week5_day1/algorithm/RightTriangle.java
@@ -0,0 +1,17 @@
+package week5_day1.algorithm;
+
+import java.util.Scanner;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
