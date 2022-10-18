b1da71599f60b47fbf33a8f0528ab05f3be21892
DongYeon Kang <myjjue00@gmail.com>
2022-10-18 09:50:29 +0900

    Create RecursiveStar.java

--- src/week5_day2/algorithm/RecursiveStar.java
+++ src/week5_day2/algorithm/RecursiveStar.java
@@ -8,15 +8,19 @@ public class RecursiveStar {
     public RecursiveStar(int height) {
         this.height = height;
     }
-
+    public void printLine(int n) {
+        if (n == 0) {
+            return;
+        }
+        printLine(n-1);
+        System.out.print("*");
+    }
     public void printStar(int n) {
         if (n == 0) {
             return;
         }
         printStar(n-1);
-        for (int i = 0; i < n; i++) {
-            System.out.print("*");
-        }
+        printLine(n);
         System.out.println();
     }
     public static void main(String[] args) {
