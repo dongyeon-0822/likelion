a7560ecd7130c35ea6115f3f2c6ca1720b8300b6
DongYeon Kang <myjjue00@gmail.com>
2022-10-18 13:37:28 +0900

    Modify UserDaoTest.java

--- src/test/java/com/dbexercise/UserDaoTest.java
+++ src/test/java/com/dbexercise/UserDaoTest.java
@@ -1,21 +1,21 @@
 package com.dbexercise;
 
+import com.dbexercise.Dao.LocalUserDao;
+import com.dbexercise.Dao.UserDao;
 import com.dbexercise.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class UserDaoTest {
     @Test
     void addAndSelect() throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDao();
-        User user = new User("5", "Jully", "1234");
+        LocalUserDao userDao = new LocalUserDao();
+        User user = new User("6", "Jully", "1234");
         userDao.add(user);
 
-        User selectedUser = userDao.get("8");
+        User selectedUser = userDao.get("6");
         Assertions.assertEquals("Jully", selectedUser.getName());
     }
 }
