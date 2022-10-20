0a0eb1a3d4077a41862bcb35f65537ffb20df552
DongYeon Kang <myjjue00@gmail.com>
2022-10-20 13:35:17 +0900

    Refactor test code

--- spring-practice/src/test/java/dao/UserDaoTest.java
+++ spring-practice/src/test/java/dao/UserDaoTest.java
@@ -23,10 +23,32 @@ class UserDaoTest {
     public void insertAndSelectTest() throws SQLException, ClassNotFoundException {
         UserDao userDao = context.getBean("localUserDao", UserDao.class);
 
+        // delete, getCount
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
         User user = new User("11", "Tom", "1234");
         userDao.insert(user);
 
         User selectedUser = userDao.select("11");
-        Assertions.assertEquals("Tom", selectedUser.getName());
+        Assertions.assertEquals(user.getName(), selectedUser.getName());
+    }
+
+    @Test
+    void count() throws SQLException, ClassNotFoundException {
+        User user1 = new User("12", "Billy", "1234");
+        User user2 = new User("13", "sujin", "1234");
+        User user3 = new User("14", "sohyun", "1234");
+
+        UserDao userDao = context.getBean("localUserDao", UserDao.class);
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
+        userDao.insert(user1);
+        assertEquals(1, userDao.getCount());
+        userDao.insert(user2);
+        assertEquals(2, userDao.getCount());
+        userDao.insert(user3);
+        assertEquals(3, userDao.getCount());
     }
 }
