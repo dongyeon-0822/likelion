702bab2e6c49eadddfd6b0e2012049beb39d60d3
DongYeon Kang <myjjue00@gmail.com>
2022-10-20 09:38:17 +0900

    Create Stack02.java, Stack02Test.java

---
+++ java-exercise/src/week5_day4/algorithm/Stack02.java
@@ -0,0 +1,41 @@
+package week5_day4.algorithm;
+
+public class Stack02 {
+    private int[] arr = new int[10000];
+    private int top = -1;
+
+    public Stack02() {
+    }
+
+    public Stack02(int size) {
+        this.arr = new int[size];
+    }
+
+    public void push(int value) {
+        if (isFull()) {
+            System.out.println("Stack is Full");
+            return;
+        }
+        this.arr[++top] = value;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return arr[top--];
+    }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+
+    public boolean isFull() {
+        return (top == arr.length-1);
+    }
+
+    public int[] getArr() {
+        return arr;
+    }
+
+}
---
+++ java-exercise/src/week5_day4/algorithm/Stack02Test.java
@@ -0,0 +1,29 @@
+package week5_day4.algorithm;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Stack02Test {
+    Stack02 stack02 = new Stack02();
+    @Test
+    void pushTest() {
+        stack02.push(1);
+        stack02.push(2);
+        assertEquals(2,stack02.getArr()[1]);
+        assertEquals(1,stack02.getArr()[0]);
+    }
+
+    @Test
+    void popTest() {
+        assertEquals(2,stack02.pop());
+    }
+
+    public static void main(String[] args) {
+        Stack02Test stack02Test = new Stack02Test();
+        stack02Test.pushTest();
+        stack02Test.popTest();
+    }
+
+}
