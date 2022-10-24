fcf8653f6b3d5d32254ae3ff4a0fcf741b3c7774
DongYeon Kang <myjjue00@gmail.com>
2022-10-24 11:19:08 +0900

    Refactor UserDaoTest.java

--- spring-practice-02/src/test/java/com/likelion/dao/UserDaoTest.java
+++ spring-practice-02/src/test/java/com/likelion/dao/UserDaoTest.java
@@ -1,6 +1,7 @@
 package com.likelion.dao;
 
 import com.likelion.domain.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,14 +18,27 @@ class UserDaoTest {
     @Autowired
     ApplicationContext context;
 
+    private UserDao userDao;
+    private User user1;
+    private User user2;
+    private User user3;
+
+    @BeforeEach
+    void serUp() {
+        this.userDao = context.getBean("localuserDao", UserDao.class);
+        this.user1 = new User("12", "Billy", "1234");
+        this.user2 = new User("13", "sujin", "1234");
+        this.user3 = new User("14", "sohyun", "1234");
+    }
+
     @Test
     void addAndGet() {
-        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
-        String id = "29";
-        userDao.add(new User(id, "EternityHwan", "1234"));
-        User user = userDao.findById(id);
+        userDao.deleteAll();
+        userDao.add(user1);
+        User user = userDao.findById(user1.getId());
 
-        assertEquals("EternityHwan", user.getName());
-        assertEquals("1234", user.getPassword());
+        assertEquals(user1.getName(), user.getName());
+        assertEquals(user1.getPassword(), user.getPassword());
     }
+
 }
