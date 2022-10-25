159f1dce4ad34747ef9650e0c6d00be839e8e36b
DongYeon Kang <myjjue00@gmail.com>
2022-10-25 15:36:09 +0900

    Add getAllTest()

--- spring-practice-02/src/test/java/com/likelion/dao/UserDaoTest.java
+++ spring-practice-02/src/test/java/com/likelion/dao/UserDaoTest.java
@@ -6,8 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.sql.SQLException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,21 +28,52 @@ class UserDaoTest {
     private User user3;
 
     @BeforeEach
-    void serUp() {
-        this.userDao = context.getBean("localuserDao", UserDao.class);
+    void setUp() {
+        this.userDao = context.getBean("localUserDao", UserDao.class);
         this.user1 = new User("12", "Billy", "1234");
         this.user2 = new User("13", "sujin", "1234");
         this.user3 = new User("14", "sohyun", "1234");
     }
 
     @Test
-    void addAndGet() {
+    void addAndGet() throws SQLException, ClassNotFoundException {
         userDao.deleteAll();
         userDao.add(user1);
         User user = userDao.findById(user1.getId());
 
         assertEquals(user1.getName(), user.getName());
         assertEquals(user1.getPassword(), user.getPassword());
+    }
+    @Test
+    void deleteAndCountTest() throws SQLException, ClassNotFoundException {
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
+        userDao.add(user1);
+        assertEquals(1, userDao.getCount());
+        userDao.add(user2);
+        assertEquals(2, userDao.getCount());
+        userDao.add(user3);
+        assertEquals(3, userDao.getCount());
+    }
+    @Test
+    void selectExceptionTest() {
+        assertThrows(EmptyResultDataAccessException.class, ()->{
+            userDao.findById("30");
+        });
+    }
+
+    @Test
+    void getAllUserTest() {
+        userDao.deleteAll();
+        List<User> userList = userDao.getAll();
+        assertEquals(0, userList.size());
+        userDao.add(user1);
+        userDao.add(user2);
+        userDao.add(user3);
+        userList = userDao.getAll();
+        assertEquals(3, userList.size());
+
     }
 
 }
