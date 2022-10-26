8eeb3a61f22f3df35739fe96b916d474b7a159c8
DongYeon Kang <myjjue00@gmail.com>
2022-10-26 10:48:40 +0900

    Create HashTable.java - 충돌해결

---
+++ java-exercise/src/week6/day3/HashTable.java
@@ -0,0 +1,91 @@
+package week6.day3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashTable {
+    // 내부 클래스 선언
+    class Node {
+        private String key;
+        private Integer value;
+
+        public Node(String key, Integer value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+    }
+    // 멤버변수
+    private int size;
+    private List<Node>[] table;
+
+    // 디폴트 생성자
+    public HashTable() {
+        this.size = 10000;
+        this.table = new ArrayList[size];
+    }
+    // 생성자 오버로딩
+    public HashTable(int size) {
+        this.size = size;
+        this.table = new ArrayList[size];
+    }
+
+    public int hash(String str) {
+        int ascii = 0;
+        for (int i = 0; i < str.length(); i++) {
+            ascii += str.charAt(i);
+        }
+        return ascii % size; // 1000개
+    }
+
+    public Integer get(String key) {
+        List<Node> nodes = this.table[hash(key)];
+        for (Node node : nodes) {
+            if (key.equals(node.getKey())) {
+                return node.value;
+            }
+        }
+
+        return null;
+    }
+
+    public void insert(String key, Integer value) {
+        int hashCode = hash(key);
+        if (this.table[hashCode] == null) {
+            this.table[hashCode] = new ArrayList<>();
+        }
+        this.table[hashCode].add(new Node(key,value));
+    }
+
+    public static void main(String[] args) {
+        String[] names = new String[]{"DongyeonKang",
+                "SubinKang", "KwanwunKo", "HyunseokKo", "KyoungdukKoo", "YeonjiGu", "SoyeonKown", "OhsukKwon", "GunwooKim", "KiheonKim", "NayeongKim", "DohyeonKim", "MinkyoungKim", "MinjiKim", "SanghoKim", "SolbaeKim", "YejinKim", "EungjunKim", "JaegeunKim", "JeonghyeonKim", "JunhoKim", "JisuKim", "kimjinah", "HaneulKim", "HeejungKim", "KimoonPark", "EunbinPark", "JeongHoonPark", "JeminPark", "TaegeunPark", "JiwonBae", "SeunggeunBaek", "JihwanByeon", "HeungseopByeon", "JeongHeeSeo", "TaegeonSeo", "SeeYunSeok", "SuyeonSeong", "SeyoelSon", "MinjiSong", "JinwooSong", "hyunboSim", "SominAhn", "JiyoungAhn", "ChangbumAn", "SoonminEom",
+                "HyeongsangOh", "SuinWoo", "JuwanWoo", "InkyuYoon", "GahyunLee", "DaonLee", "DohyunLee", "SanghunLee", "SujinLee", "AjinLee", "YeonJae", "HyeonjuLee", "HakjunYim", "SeoyunJang", "SeohyeonJang", "JinseonJang", "SujinJeon", "SeunghwanJeon", "DaehwanJung", "JaeHyunJeung", "HeejunJeong", "GukhyeonCho", "MunjuJo", "YejiJo", "ChanminJu", "MinjunChoi", "SujeongChoi", "SeunghoChoi", "AyeongChoi", "GeonjooHan", "JinhyuckHeo", "MinwooHwang", "SieunHwang",
+                "JunhaHwang"};
+
+        HashTable hashTable = new HashTable(200);
+        hashTable.insert("Yoonseo", 1);
+        hashTable.insert("Seoyoon", 2);
+
+        int result = hashTable.get("Yoonseo");
+        if (result == 1) {
+            System.out.println("테스트 성공");
+        } else {
+            System.out.printf("테스트 실패 value:%d", result);
+        }
+
+        result = hashTable.get("Seoyoon");
+        if (result == 2) {
+            System.out.println("테스트 성공");
+        } else {
+            System.out.printf("테스트 실패 value:%d", result);
+        }
+    }
+}
