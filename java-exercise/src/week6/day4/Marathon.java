9ab1311aeea4b1cedbc3798dbf704cba629d915b
DongYeon Kang <myjjue00@gmail.com>
2022-10-27 09:16:47 +0900

    Create Marathon - Solved by sorting

---
+++ java-exercise/src/week6/day4/Marathon.java
@@ -0,0 +1,16 @@
+package week6.day4;
+
+import java.util.Arrays;
+
+public class Marathon {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i;
+        for(i = 0; i<completion.length; i++) {
+            if (!participant[i].equals(completion[i]))
+                return participant[i];
+        }
+        return participant[i];
+    }
+}
