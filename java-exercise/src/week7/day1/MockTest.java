809241e22b07fd1b2c0475d6be70a5cb047ac6d3
DongYeon Kang <myjjue00@gmail.com>
2022-10-31 10:08:02 +0900

    Fix MockTest.java - solved runtime error

--- java-exercise/src/week7/day1/MockTest.java
+++ java-exercise/src/week7/day1/MockTest.java
@@ -7,36 +7,36 @@ import java.util.List;
 
 public class MockTest {
     public int[] solution(int[] answers) {
-        List<Integer> list = new ArrayList<>();
+        int[] answer = {};
         int[] stu1 = {1,2,3,4,5};
         int[] stu2 = {2,1,2,3,2,4,2,5};
         int[] stu3 = {3,3,1,1,2,2,4,4,5,5};
         int[] cnt = {0,0,0};
 
         for (int i = 0; i < answers.length; i++) {
-            if (stu1[i] == answers[i % stu1.length]) {
+            if (answers[i] == stu1[i % stu1.length]) {
                 cnt[0] += 1;
             }
-            if (stu2[i] == answers[i % stu2.length]) {
+            if (answers[i] == stu2[i % stu2.length]) {
                 cnt[1] += 1;
             }
-            if (stu3[i] == answers[i % stu3.length]) {
+            if (answers[i] == stu3[i % stu3.length]) {
                 cnt[2] += 1;
             }
         }
-        Arrays.sort(cnt);
-        int max_cnt = cnt[2];
 
-        for (int i = 0; i <cnt.length; i++) {
-            if (cnt[i] == max_cnt) {
-                list.add(i+1);
-            }
-        }
-        int[] answer = new int[list.size()];
-        for (int i = 0 ; i < list.size() ; i++) {
-            answer[i] = list.get(i).intValue();
+        int max_cnt = (cnt[0]> cnt[1]) ? cnt[0] : cnt[1];
+        max_cnt = (max_cnt> cnt[2]) ? max_cnt : cnt[2];
+
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i <3; i++) {
+            if (cnt[i] == max_cnt)
+                arr.add(i+1);
         }
 
+        answer = new int[arr.size()];
+        for (int i = 0 ; i < arr.size() ; i++)
+            answer[i] = arr.get(i).intValue();
         return answer;
     }
 }
