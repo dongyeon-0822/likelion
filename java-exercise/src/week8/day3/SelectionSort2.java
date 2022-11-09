abf9e196105f1edf2698ba2111ab4a42788cdf6e
DongYeon Kang <myjjue00@gmail.com>
2022-11-09 10:28:44 +0900

    Refector SelectionSort2.java - use interface lambda

--- java-exercise/src/week8/day3/SelectionSort2.java
+++ java-exercise/src/week8/day3/SelectionSort2.java
@@ -1,22 +1,30 @@
 package week8.day3;
 
-public class SelectionSort2 {
-    public static void main(String[] args) {
-        int arr[] = new int[]{2, 3, 4, 9, 10, 223, 111, 23, 7, 39};
+import java.util.Arrays;
 
+interface StatementStrategy {
+    boolean apply(int a, int b);
+}
+public class SelectionSort2 {
+    public int[] selectionSort(int[] arr, StatementStrategy stmt) {
         for (int i = 0; i < arr.length - 1; i++) {
-            int max_idx = i;
-            for (int j = i+1; j < arr.length; j++) {
-                if (arr[j] > arr[max_idx]) {
-                    max_idx = j;
-                }
+            int idx = i;
+            for (int j = i; j < arr.length; j++) {
+                if (stmt.apply(arr[idx], arr[j]))
+                    idx = j;
             }
-            int tmp = arr[i];
-            arr[i] = arr[max_idx];
-            arr[max_idx] = tmp;
+            int temp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = temp;
+            System.out.println(Arrays.toString(arr));
         }
-        for (int i : arr) {
-            System.out.println(i);
-        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+        SelectionSort2 selectionSort = new SelectionSort2();
+        selectionSort.selectionSort(arr, (a, b) -> a < b); // 오름차순
+        selectionSort.selectionSort(arr, (a, b) -> a > b); // 내림차순
     }
 }
