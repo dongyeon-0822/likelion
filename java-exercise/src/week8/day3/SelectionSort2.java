7ed09310635bc0996cd7a433a57403e446cdf049
DongYeon Kang <myjjue00@gmail.com>
2022-11-09 10:51:19 +0900

    Refector SelectionSort2.java - use Function, BiFunction

--- java-exercise/src/week8/day3/SelectionSort2.java
+++ java-exercise/src/week8/day3/SelectionSort2.java
@@ -1,21 +1,22 @@
 package week8.day3;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 interface StatementStrategy {
     boolean apply(int a, int b);
 }
 public class SelectionSort2 {
-    public int[] selectionSort(int[] arr, StatementStrategy stmt) {
+    public int[] selectionSort(int[] arr, BiFunction<Integer, Integer, Boolean> stmt) {
         for (int i = 0; i < arr.length - 1; i++) {
-            int idx = i;
+            int swapIdx = i;
             for (int j = i; j < arr.length; j++) {
-                if (stmt.apply(arr[idx], arr[j]))
-                    idx = j;
+                if (stmt.apply(arr[swapIdx], arr[j])) swapIdx = j;
             }
             int temp = arr[i];
-            arr[i] = arr[idx];
-            arr[idx] = temp;
+            arr[i] = arr[swapIdx];
+            arr[swapIdx] = temp;
             System.out.println(Arrays.toString(arr));
         }
         return arr;
@@ -24,7 +25,16 @@ public class SelectionSort2 {
     public static void main(String[] args) {
         int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
         SelectionSort2 selectionSort = new SelectionSort2();
-        selectionSort.selectionSort(arr, (a, b) -> a < b); // 오름차순
-        selectionSort.selectionSort(arr, (a, b) -> a > b); // 내림차순
+        // interface 사용
+        // selectionSort.selectionSort(arr, (a, b) -> a < b); // 오름차순
+        // selectionSort.selectionSort(arr, (a, b) -> a > b); // 내림차순
+
+        // Function 사용
+        //Function<Integer[], Boolean> fn = (arr1) -> arr1[0] > arr1[1];
+        //System.out.println(fn.apply(new Integer[]{10, 20}));
+
+        // BiFunction 사용
+        BiFunction<Integer, Integer, Boolean> biFunction = (a, b) -> a > b;
+        selectionSort.selectionSort(arr, biFunction);
     }
 }
