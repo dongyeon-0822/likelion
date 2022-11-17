4abda0ee5f61632c00f3f553c9895f580af6372d
DongYeon Kang <myjjue00@gmail.com>
2022-11-17 09:45:36 +0900

    feat: quickSort 교환 반복기능 추가

--- java-exercise/src/week9/day4/QuickSort2.java
+++ java-exercise/src/week9/day4/QuickSort2.java
@@ -5,17 +5,20 @@ public class QuickSort2 {
         int pivot = arr[arr.length/2]; //pivot 뽑기
         int leftIdx = 0;
         int rightIdx = arr.length-1;
-        while (arr[leftIdx] < pivot)
-            leftIdx++;
-        while (arr[leftIdx] > pivot)
-            rightIdx--;
-        if (leftIdx <= rightIdx) {
-            int tmp = arr[leftIdx];
-            arr[leftIdx] = arr[rightIdx];
-            arr[rightIdx] = tmp;
-            leftIdx++;
-            rightIdx--;
+        while (leftIdx <= rightIdx) {
+            while (arr[leftIdx] < pivot)
+                leftIdx++;
+            while (arr[rightIdx] > pivot)
+                rightIdx--;
+            if (leftIdx <= rightIdx) {
+                int tmp = arr[leftIdx];
+                arr[leftIdx] = arr[rightIdx];
+                arr[rightIdx] = tmp;
+                leftIdx++;
+                rightIdx--;
+            }
         }
+
     }
     public static void main(String[] args) {
         int[] arr =  arr = new int[]{20, 18, 5, 19, 40, 50, 5, 25};
