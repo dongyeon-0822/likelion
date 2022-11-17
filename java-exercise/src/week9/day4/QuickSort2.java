1bcc18601247276b0a426dca1eaba27712dc13ef
DongYeon Kang <myjjue00@gmail.com>
2022-11-17 10:20:34 +0900

    feat: quickSort 구현 완료

--- java-exercise/src/week9/day4/QuickSort2.java
+++ java-exercise/src/week9/day4/QuickSort2.java
@@ -1,10 +1,12 @@
 package week9.day4;
 
+import java.util.Arrays;
+
 public class QuickSort2 {
-    public void quickSort(int[] arr) {
-        int pivot = arr[arr.length/2]; //pivot 뽑기
-        int leftIdx = 0;
-        int rightIdx = arr.length-1;
+    public int[] quickSort(int[] arr, int start, int end) {
+        int pivot = arr[(start+end)/2]; // pivot 뽑기
+        int leftIdx = start;
+        int rightIdx = end;
         while (leftIdx <= rightIdx) {
             while (arr[leftIdx] < pivot)
                 leftIdx++;
@@ -18,9 +20,14 @@ public class QuickSort2 {
                 rightIdx--;
             }
         }
-
+        if (start<rightIdx) quickSort(arr,start,rightIdx);
+        if (leftIdx<end) quickSort(arr,leftIdx,end);
+        return arr;
     }
     public static void main(String[] args) {
-        int[] arr =  arr = new int[]{20, 18, 5, 19, 40, 50, 5, 25};
+        int[] arr =  arr = new int[]{20, 18, 5, 19, 40, 50, 52, 25};
+        QuickSort2 quickSort2 = new QuickSort2();
+        int[] new_arr = quickSort2.quickSort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(new_arr));
     }
 }
