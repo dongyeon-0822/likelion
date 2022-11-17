68c2c6400737c8ce882f782b1009d18dbc2254f9
DongYeon Kang <myjjue00@gmail.com>
2022-11-17 09:34:48 +0900

    feat: quickSort 교환기능 추가

---
+++ java-exercise/src/week9/day4/QuickSort2.java
@@ -0,0 +1,23 @@
+package week9.day4;
+
+public class QuickSort2 {
+    public void quickSort(int[] arr) {
+        int pivot = arr[arr.length/2]; //pivot 뽑기
+        int leftIdx = 0;
+        int rightIdx = arr.length-1;
+        while (arr[leftIdx] < pivot)
+            leftIdx++;
+        while (arr[leftIdx] > pivot)
+            rightIdx--;
+        if (leftIdx <= rightIdx) {
+            int tmp = arr[leftIdx];
+            arr[leftIdx] = arr[rightIdx];
+            arr[rightIdx] = tmp;
+            leftIdx++;
+            rightIdx--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr =  arr = new int[]{20, 18, 5, 19, 40, 50, 5, 25};
+    }
+}
