9a17587cef9e234c8354e97e6d00ddd1c6c8c4e9
DongYeon Kang <myjjue00@gmail.com>
2022-11-22 09:28:44 +0900

    feat: 1-100 합 구하는 로직 추가

--- java-exercise/src/week10/day1/RecursivePractice.java
+++ java-exercise/src/week10/day1/RecursivePractice.java
@@ -1,14 +1,14 @@
 package week10.day1;
 
 public class RecursivePractice {
-    public static int print100(int n) {
-        if (n > 100) {
-            return 0;
-        }
-        System.out.println(n);
-        return print100(n+1);
+    public static int sum = 0;
+    public static void print100(int n) {
+        if (n > 100) return;
+        sum += n;
+        print100(n+1);
     }
     public static void main(String[] args) {
         print100(1);
+        System.out.println(sum);
     }
 }
