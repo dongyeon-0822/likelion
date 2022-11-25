b1c8354957f7b6d8f3fa1564339bc115a5b94800
DongYeon Kang <myjjue00@gmail.com>
2022-11-25 09:46:02 +0900

    feat: Update CodeUp_2623
    - GCD 재귀없이 풀이

--- java-exercise/src/week10/day4/CodeUp_2623.java
+++ java-exercise/src/week10/day4/CodeUp_2623.java
@@ -3,19 +3,31 @@ package week10.day4;
 import java.util.Scanner;
 
 public class CodeUp_2623 {
+    static int gcd2(int a, int b) {
+        while (a != b) {
+            if (a > b)
+                a = a-b;
+            else
+                b = b-a;
+        }
+        return a;
+    }
+
     static int gcd(int a, int b) {
         if (a == b) {
             return a;
         } else if (a > b) {
             return gcd(a - b, b);
         } else {
-            return gcd(b-a,a);
+            return gcd(b - a, a);
         }
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        System.out.println(gcd(a,b));
+        System.out.println(gcd(a, b));
+        System.out.println(gcd2(a, b));
     }
 }
