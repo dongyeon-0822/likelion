cd618e8fdc25ef9be743c9c724162eb61fdfcd5c
DongYeon Kang <myjjue00@gmail.com>
2022-11-28 09:21:14 +0900

    feat: 알파벳조합 출력2

--- java-exercise/src/week11/day1/AlphabetComb.java
+++ java-exercise/src/week11/day1/AlphabetComb.java
@@ -2,12 +2,20 @@ package week11.day1;
 
 public class AlphabetComb {
     static void printComb() {
-        char c = 'A';
         for (int i = 0; i < 26; i++) {
-            System.out.println((char)(c+i));
+            System.out.println((char)('A'+i));
+        }
+    }
+    static void printComb2() {
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 26; j++) {
+                System.out.print((char)('A'+i));
+                System.out.println((char)('A'+j));
+            }
         }
     }
     public static void main(String[] args) {
-        printComb();
+        //printComb();
+        printComb2();
     }
 }
