f2bb4b5caf0514784dc78a466748f5db84bcfe2c
DongYeon Kang <myjjue00@gmail.com>
2022-11-30 09:48:31 +0900

    feat: Create FindParent

---
+++ java-exercise/src/week11/day3/FindParent.java
@@ -0,0 +1,12 @@
+package week11.day3;
+
+public class FindParent {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+
+        int parentA = Math.floorDiv(3,2);
+
+        System.out.println(parentA);
+    }
+}
