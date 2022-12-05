9fa92541bf3805f4c24eda5e0640288908afde44
DongYeon Kang <myjjue00@gmail.com>
2022-12-05 10:58:43 +0900

    feat: 부모인덱스가 자식인덱스보다 큰 경우 swap하는 로직 추가

--- java-exercise/src/week12/day1/Heap.java
+++ java-exercise/src/week12/day1/Heap.java
@@ -1,6 +1,14 @@
 package week12.day1;
 
+import java.util.Arrays;
+
 public class Heap {
+    private int[] heapedArr;
+
+    public Heap(int[] heapedArr) {
+        this.heapedArr = heapedArr;
+    }
+
     public int getParentIdx(int index) {
         return index/2;
     }
@@ -10,8 +18,22 @@ public class Heap {
     public int getRightChildIdx(int index) {
         return index*2+1;
     }
-    public static void main(String[] args) {
-        int[] arr = new int[]{6,5,7,8};
 
+    private void makeHeap(int idx) {
+        int parentIdx = getParentIdx(idx);
+        while (heapedArr[parentIdx] > heapedArr[idx]) {
+            int tmp = heapedArr[idx];
+            heapedArr[idx] = heapedArr[parentIdx];
+            heapedArr[parentIdx] = tmp;
+            parentIdx = getParentIdx(idx);
+        }
+    }
+    public void printArray() {
+        System.out.println(Arrays.toString(heapedArr));
+    }
+    public static void main(String[] args) {
+        Heap heap = new Heap(new int[]{6, 5, 7, 8});
+        heap.makeHeap(1);
+        heap.printArray();
     }
 }
