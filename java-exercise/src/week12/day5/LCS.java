a7ab7c295e306a4dc702a80bbdd1b5c9e8246864
DongYeon Kang <myjjue00@gmail.com>
2022-12-09 10:21:10 +0900

    feat: Create LCS.java
    - LCS 로직 구현 완료

--- java-exercise/src/week12/day5/LCS.java
+++ java-exercise/src/week12/day5/LCS.java
@@ -5,11 +5,18 @@ public class LCS {
         String str1 = "ABCDEF";
         String str2 = "DCABDC";
 
-        int[][] dp = new int[str2.length()][str1.length()];
+        int[][] dp = new int[str2.length()+1][str1.length()+1];
 
         for (int i = 0; i <str2.length(); i++) {
             for (int j = 0; j < str1.length(); j++) {
                 System.out.printf("i:%s j:%s\n", str2.charAt(i), str1.charAt(j));
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+                else if (str2.charAt(i) == str1.charAt(j))
+                    dp[i][j] = dp[i-1][j-1]+1;
+                else {
+                    dp[i][j] = (dp[i-1][j] > dp[i][j-1] ? dp[i-1][j] : dp[i][j-1]);
+                }
             }
         }
     }
